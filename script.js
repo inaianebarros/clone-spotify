739890473f3282d9c5eafbf9e4cd3a445f1da52d
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-02-11 22:44:34 -0300

    Incluir mais cards na pagina do spotify

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 // npx json-server --watch api-artists/artists.json --port 3000
 
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const playlistContainer = document.getElementById('result-playlists');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
 
-//funcao para consumir a api, usar o paramento que foi o que digitamos
-function requestApi(searchTerm){
-    const url = `http://localhost:3000/artists/?name_like=${searchTerm}`
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((results) => displayResults(results));
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(results) {
-    playlistContainer.classList.add("hidden");
-    const artistImage = document.getElementById("artist-img");
-    const artistName = document.getElementById("artist-name");
-  
-    results.forEach((element) => {
-      artistImage.src = element.urlImg;
-      artistName.innerText = element.name;
-    });
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores    
 
-    resultArtist.classList.remove("hidden");
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm) );
+
+
+  filteredArtists.forEach(artist => {    
+      const artistCard = document.createElement('div');
+      artistCard.classList.add('artist-card');
+
+      artistCard.innerHTML = `
+          <div class="card-img">
+              <img class="artist-img" src="${artist.urlImg}" />
+              <div class="play">
+                  <span class="fa fa-solid fa-play"></span>
+              </div>
+          </div>
+      <div class="card-text">              
+              <span class="artist-name">${artist.name}</span>
+              <span class="artist-categorie">Artista</span>
+          </div>
+      `;
+       gridContainer.appendChild(artistCard);
+  });
+
+  resultArtist.classList.remove('hidden');
 }
-  
 
-document.addEventListener("input", function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-      resultArtist.classList.add("hidden");
-      playlistContainer.classList.remove("hidden");
-      return;
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase().trim();
+
+    if (searchTerm === '') {
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+        return;
     }
+
     requestApi(searchTerm);
 });
